09e58b1022c2f6df697559251d605b1ad0b33843
zhaowenjie <zwj27332@icloud.com>
2022-06-20 00:05:47 +0800

    my first go code

---
+++ go_study/a01_test.go
@@ -0,0 +1,11 @@
+package go_study
+
+import (
+	"fmt"
+	"testing"
+)
+//test方式运行函数
+func Test_a01_01(t *testing.T) {
+	fmt.Println("okokokokok")
+	fmt.Println("1234")
+}
---
+++ go_study/main/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main01(){
+	fmt.Println("zhaowenjie ain function")
+	fmt.Println("zhaowenjie ain function")
+	fmt.Println("zhaowenjie ain function")
+	fmt.Println("zhaowenjie ain function")
+	fmt.Println("zhaowenjie ain function")
+
+}
+func main() {
+}
