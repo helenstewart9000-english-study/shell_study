9a800736defdc6d478ed6643a765bd74e3ea2df2
zhaowenjie <zwj27332@icloud.com>
2022-06-20 00:25:23 +0800

    my first go code

--- go_study/main/main.go
+++ go_study/main/main.go
@@ -8,6 +8,7 @@ func main01(){
 	fmt.Println("zhaowenjie ain function")
 	fmt.Println("zhaowenjie ain function")
 	fmt.Println("zhaowenjie ain function")
+	fmt.Println("zhaowenjie ain function")
 
 }
 func main() {
